ce394a0b7b0165d409f2cfd7dc94c04a2131d7e9
Nhatcapdang <54816813+Nhatcapdang@users.noreply.github.com>
2023-12-29 16:58:15 +0700

    react-navigation/bottom-tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,33 @@
 /** @format */
 
+import { AntDesign, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {
+  BottomTabBarProps,
+  BottomTabNavigationOptions,
+} from '@react-navigation/bottom-tabs';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import React, { useEffect, useReducer, useRef } from 'react';
+import {
+  LayoutChangeEvent,
+  Pressable,
+  useColorScheme,
+  StyleSheet,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from '../../components/Themed';
 import Colors from '../../constants/Colors';
+// navigation
+// svg
+import Svg, { Path } from 'react-native-svg';
 
+// reanimated
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+  withTiming,
+} from 'react-native-reanimated';
+// lottie
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
@@ -24,9 +46,11 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
       }}
+      tabBar={(props) => <AnimatedTabBar {...props} />}
+      initialRouteName="three"
     >
       <Tabs.Screen
-        name="index"
+        name="one"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => (
@@ -56,12 +80,207 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="Analytics"
+        name="three"
         options={{
-          title: 'Analytics',
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="arrow-expand-all"
+              size={24}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="four"
+        options={{
+          title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="five"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => (
+            <Feather name="hexagon" size={24} color="black" />
+          ),
         }}
       />
     </Tabs>
   );
 }
+const PlaceholderScreen = () => {
+  return <View style={{ flex: 1, backgroundColor: '#604AE6' }} />;
+};
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+const AnimatedTabBar = ({
+  state: { index: activeIndex, routes },
+  navigation,
+  descriptors,
+}: BottomTabBarProps) => {
+  const { bottom } = useSafeAreaInsets();
+
+  // get information about the components position on the screen -----
+
+  const reducer = (state: any, action: { x: number; index: number }) => {
+    // Add the new value to the state
+    return [...state, { x: action.x, index: action.index }];
+  };
+
+  const [layout, dispatch] = useReducer(reducer, []);
+  console.log(layout);
+
+  const handleLayout = (event: LayoutChangeEvent, index: number) => {
+    dispatch({ x: event.nativeEvent.layout.x, index });
+  };
+
+  // animations ------------------------------------------------------
+
+  const xOffset = useDerivedValue(() => {
+    // Our code hasn't finished rendering yet, so we can't use the layout values
+    if (layout.length !== routes.length) return 0;
+    // We can use the layout values
+    // Copy layout to avoid errors between different threads
+    // We subtract 25 so the active background is centered behind our TabBar Components
+    // 20 pixels is the width of the left part of the svg (the quarter circle outwards)
+    // 5 pixels come from the little gap between the active background and the circle of the TabBar Components
+    return [...layout].find(({ index }) => index === activeIndex)!.x - 25;
+    // Calculate the offset new if the activeIndex changes (e.g. when a new tab is selected)
+    // or the layout changes (e.g. when the components haven't finished rendering yet)
+  }, [activeIndex, layout]);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      // translateX to the calculated offset with a smooth transition
+      transform: [{ translateX: withTiming(xOffset.value, { duration: 250 }) }],
+    };
+  });
+
+  return (
+    <View style={[styles.tabBar, { paddingBottom: bottom }]}>
+      <AnimatedSvg
+        width={110}
+        height={60}
+        viewBox="0 0 110 60"
+        style={[styles.activeBackground, animatedStyles]}
+      >
+        <Path
+          fill="#fff"
+          d="M20 0H0c11.046 0 20 8.953 20 20v5c0 19.33 15.67 35 35 35s35-15.67 35-35v-5c0-11.045 8.954-20 20-20H20z"
+        />
+      </AnimatedSvg>
+
+      <View style={styles.tabBarContainer}>
+        {routes.map((route, index) => {
+          const active = index === activeIndex;
+          const { options } = descriptors[route.key];
+
+          return (
+            <TabBarComponent
+              key={route.key}
+              active={active}
+              options={options}
+              onLayout={(e) => handleLayout(e, index)}
+              onPress={() => navigation.navigate(route.name)}
+            />
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+type TabBarComponentProps = {
+  active?: boolean;
+  options: BottomTabNavigationOptions;
+  onLayout: (e: LayoutChangeEvent) => void;
+  onPress: () => void;
+};
+const TabBarComponent = ({
+  active,
+  options,
+  onLayout,
+  onPress,
+}: TabBarComponentProps) => {
+  // handle lottie animation -----------------------------------------
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (active && ref?.current) {
+      // @ts-ignore
+      ref.current.play();
+    }
+  }, [active]);
+
+  // animations ------------------------------------------------------
+
+  const animatedComponentCircleStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: withTiming(active ? 1 : 0, { duration: 250 }),
+        },
+      ],
+    };
+  });
+
+  const animatedIconContainerStyles = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(active ? 1 : 0.5, { duration: 250 }),
+    };
+  });
+
+  return (
+    <Pressable onPress={onPress} onLayout={onLayout} style={styles.component}>
+      <Animated.View
+        style={[styles.componentCircle, animatedComponentCircleStyles]}
+      />
+      <Animated.View
+        style={[styles.iconContainer, animatedIconContainerStyles]}
+      >
+        {/* @ts-ignore */}
+        {options.tabBarIcon ? options.tabBarIcon({ ref }) : <Text>?</Text>}
+      </Animated.View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: 'red',
+  },
+  activeBackground: {
+    position: 'absolute',
+  },
+  tabBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    backgroundColor: 'transparent',
+  },
+  component: {
+    height: 60,
+    width: 60,
+  },
+  componentCircle: {
+    flex: 1,
+    borderRadius: 30,
+    // backgroundColor: 'navy',
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  },
+  icon: {
+    height: 36,
+    width: 36,
+    color: 'white',
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
+/** @format */
+
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
